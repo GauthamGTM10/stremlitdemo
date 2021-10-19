eab786ba824b61aa1b5661ddda3c6cb44c3cb406
GauthamGTM10 <48794585+GauthamGTM10@users.noreply.github.com>
2021-10-19 11:43:11 +0000

    new_changes

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 
 import streamlit as st
 
-st.header('Welcome to HackerShrine Streamlit App')
+st.header("Hi this is demo app by gowtham")
 
-st.image('hackershrine_header-02.jpg',use_column_width = True)
-
-
-
+st.image('sitellogo.png',use_column_width = True)
 
